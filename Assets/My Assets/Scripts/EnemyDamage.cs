510fa8d3bbd008b8f5da1b272a2edea04803f4cd
chiswex <74733557+chiswex@users.noreply.github.com>
2021-12-09 01:46:32 +0100

    damage

---
+++ Assets/My Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private int dmg = 1;
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            other.gameObject.GetComponent<PlayerHealth>().DamagePlayer(dmg);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/My Assets/Scripts/PlayerHealth.cs
+++ Assets/My Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int dmg = 1;
     public int hearts = 4;
     public int maxHearts = 8;
     [SerializeField] HealthSystem hs;
@@ -14,20 +16,23 @@ public class PlayerHealth : MonoBehaviour
     }
     public void DamagePlayer (int dmg)
     {
-        if(hearts >= 0)
+        if(hearts > 0)
         {
             hearts -= dmg;
             hs.DrawHearts(hearts, maxHearts);
         }
-
+        else
+        {
+            //GameController.instance.Respawn();
+            SceneManager.LoadScene("GameOverScreen");
+        }
     }
-    public void HealPlayer (int dmg)
+    public void HealPlayer(int dmg)
     {
         if (hearts < maxHearts)
         {
             hearts += dmg;
             hs.DrawHearts(hearts, maxHearts);
-
         }
     }
 }
