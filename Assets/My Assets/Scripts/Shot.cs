fdc1d7371b25e04506ab77a610ecfd2d0dc77aed
chiswex <74733557+chiswex@users.noreply.github.com>
2021-12-09 13:59:49 +0100

    shot

---
+++ Assets/My Assets/Scripts/Shot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [HideInInspector] public float ArrowVelocity;
+    [SerializeField] Rigidbody2D rb;
+
+    private void Start()
+    {
+        Destroy(gameObject, 4f);
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * ArrowVelocity;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
